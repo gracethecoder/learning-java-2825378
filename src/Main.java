f41a2f1cb12cc3dbad9154a409e0142da9221a93
Grace <xinruw1@gmail.com>
2022-05-22 18:27:56 -0700

    finished 03_06_b

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean isOnRepeat = true;
+        while(isOnRepeat){
+            System.out.println("Playing current song");
+            System.out.println("would you like to take this song off the repeat?");
+            String userInput = input.next();
+
+            if(userInput.equals("yea")){
+                isOnRepeat = false;
+            }
+
+
+
+
+
+        }
+        System.out.println("Playing next song");
     }
+
 }
