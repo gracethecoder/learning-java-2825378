010641765079632b01ed581663a7458c3f0b19f8
Grace <xinruw1@gmail.com>
2022-05-24 12:44:31 -0700

    finished 03_07

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,32 @@
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        Scanner input = new Scanner (System.in);
+        String question = "Do you like La Tiao";
+        String choiceOne = "yesss";
+        String choiceTwo = "noooo";
+        String choiceThree = "i can try";
 
-        String correctAnswer = choiceTwo;
+        String correctAnswer = choiceOne;
 
         // Write a print statement asking the question
+        System.out.println(question);
         // Write a print statement giving the answer choices
-
+        System.out.println(choiceOne);
+        System.out.println(choiceTwo);
+        System.out.println(choiceThree);
         // Have the user input an answer
         // Retrieve the user's input
+        String userInput = input.next();
 
         // If the user's input matches the correctAnswer...
         // then the user is correct and we want to print out a congrats message to the user.
+        if (correctAnswer.equals(userInput.toLowerCase())) {
+            System.out.println("Congrats");
+        } else {
+            System.out.println("Sorry you are missing out");
+        }
 
         // If the user's input does not match the correctAnswer...
         // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
